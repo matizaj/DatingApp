657d4a14f01a77a5bf244a02bcc9041fd2fe5818
mzajac <matzaj101@gmail.com>
2018-08-13 23:13:23 +0200

    Registration process via angular

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -18,6 +18,7 @@ namespace DatingApp.API.Controllers
         public ValuesController(DataContext context)=>_context=context;
             
         // GET api/values
+         [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {   
